b8d40221e0752c5bffcb02416f84a97ab78c082c
Courtney Carroll <bcakescw@gmail.com>
2022-07-20 21:05:15 -0400

    images added to home and error page

--- views/error404.jsx
+++ views/error404.jsx
@@ -6,7 +6,12 @@ function error404 () {
       <Def>
           <main>
               <h1>404: PAGE NOT FOUND</h1>
-              <p>Oops, sorry, we can't find this page!</p>
+              <p>Oops, sorry, we can't find this page!</p>\ <div>
+                <img src="/images/puppy.jpg" alt = "Little Puppy"/>
+                <div>
+                    Photo by <a href="https://unhttps://unsplash.com/photos/fk4tiMlDFF0?utm_source=unsplash&utm_medium=referral&utm_content=creditShareLinksplash.com/photos/nTZOILVZuOg?utm_source=unsplash&utm_medium=referral&utm_content=creditShareLink">Hannah Grace</a>on<a href="https://unsplash.https://unsplash.com/photos/fk4tiMlDFF0com/s/photos/food">Unsplash</a>
+                </div>
+              </div>
           </main>
       </Def>
     )
--- views/home.jsx
+++ views/home.jsx
@@ -6,6 +6,12 @@ function home () {
       <Def>
           <main>
               <h1>HOME</h1>
+              <div>
+                <img src="/images/fruitssmall.jpg" alt = "Fruit Salad and Breakfast"/>
+                <div>
+                    Photo by <a href="https://unsplash.com/photos/nTZOILVZuOg?utm_source=unsplash&utm_medium=referral&utm_content=creditShareLink">Brooke Lark</a>on<a href="https://unsplash.com/s/photos/food">Unsplash</a>
+                </div>
+              </div>
               <a href = "/places">
                 <button className="btn-primary">Places Pages</button>
               </a>
