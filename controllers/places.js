a2a58dd348941d193a0cc2b80dc6d8b968f8a896
Courtney Carroll <bcakescw@gmail.com>
2022-07-22 22:19:50 -0400

    show route connected

--- controllers/places.js
+++ controllers/places.js
@@ -1,13 +1,31 @@
 const router = require('express').Router()
 const places = require('../models/places.js')
 
-router.get('/', (req, res) => {
-    res.render('places/index', { places })
+router.get('/', (req, res)=> {
+    res.render('places/index', {places})
 })
 
 router.get('/new', (req, res) => {
     res.render('places/new')
   })  
+
+  router.get('/', (req, res) => {
+    res.render('places/show')
+  })
+  
+router.get('/', (req, res) => {
+    let id = Number(req.params.id)
+    if (isNaN(id)) {
+      res.render('error404')
+    }
+    else if (!places[id]) {
+      res.render('error404')
+    }
+    else {
+      res.render('places/show', {places: places[id]})
+    }
+  })
+  
 
   router.post('/', (req, res) => {
     console.log(req.body)
@@ -25,9 +43,7 @@ router.get('/new', (req, res) => {
     res.redirect('POST /places')
   })
   
-  
-router.get('/:id', (req, res)=> {
-    res.render('places/index', {places})
-})
+
+
 
 module.exports = router
--- views/places/index.jsx
+++ views/places/index.jsx
@@ -1,31 +1,36 @@
 const React = require('react')
 const Def = require('../default')
 
-function Index (data) {
-    const placesFormatted = data.places.map((place) => {
+function Index(data) {
+    let placesFormatted = data.places.map((place, index) => {
         return (
-            <div className="col-sm-6">
-                <h2>{place.name}</h2>
-                <p className="text-center">
-                    {place.cuisines}
-                </p>
-                <img src={place.pic} alt = {place.name}/>
-                <p className="text-cenmter">
-                    Located in {place.city}, {place.state}
-                </p>
-            </div>
+          <div className="col-sm-6">
+            <h2>
+              <a href={`/places/${index}`} >
+                {place.name}
+              </a>
+            </h2>
+            <p className="text-center">
+              {place.cuisines}
+            </p>
+            <img src={place.pic} alt={place.name} />
+            <p className="text-center">
+              Located in {place.city}, {place.state}
+            </p>
+          </div>
         )
-    })
+      })
+      
     return (
         <Def>
             <main>Restuarant's to Rant or Rave About
-            <div className="row">
-                {placesFormatted}
+                <div className="row">
+                    {placesFormatted}
                 </div>
             </main>
         </Def>
     )
 }
-    
+
 
 module.exports = Index
